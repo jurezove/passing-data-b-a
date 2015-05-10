db66f7d6ce74d6ae284c46a7a5e395c22807355b
Jure Žove <jure.pase.kure@gmail.com>
2015-05-10 18:32:50 +0200

    Initial Commit

---
+++ Passing Data B-A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Passing Data B-A
+//
+//  Created by Jure Zove on 10/05/15.
+//  Copyright (c) 2015 Candy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
